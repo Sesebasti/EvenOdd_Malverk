5345912be241547d03b5fc5a329be78fc0d1ce9d
Sebastián Escudero <116760721+Sesebasti@users.noreply.github.com>
2026-02-04 21:40:36 -0300

    Add files via upload

---
+++ localization/default.lua
@@ -0,0 +1,17 @@
+return {
+    descriptions = {
+        texture_packs = {
+            texpack_malverk_evenodd_evenodd = {
+                name = 'EvenOdd',
+                text = {
+                    'Retextures {C:red}Even Steven{} and {C:blue}Odd Todd{}',
+                    'to be a {E:1,C:legendary}yuri couple!{}',
+                    'Art by {E:1,C:dark_edition,s:1.1}eaze',
+                    '{C:inactive,s:0.8}Code by SeSebasti.{}',
+                    'Heavily inspired by the',
+                    'code of {E:1,C:dark_edition,s:1.1}Victin{}'
+                }
+            }
+        }
+    }
+}
---
+++ main.lua
@@ -0,0 +1,16 @@
+AltTexture{
+    key = 'evenodd',
+    set = 'Joker',
+    path = 'EvenOdd.png',
+    keys = {
+        'j_even_steven',
+        'j_odd_todd'
+    },
+}
+
+TexturePack{
+    key = 'evenodd',
+    textures = {
+        'malverk_evenodd_evenodd'
+    }
+}
